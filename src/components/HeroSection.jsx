841be453d89ea66a3f738f8e3033c1bbbd4f467d
Sakshi Maheshwari <sakshicoder800@gmail.com>
2024-10-05 22:24:13 +0530

    Hero Section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import video1 from "../assets/video1.mp4";
+import video2 from "../assets/video1.mp4";
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
-  )
-}
+    <div className="flex flex-col items-center mt-6 lg:mt-20">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+        VirtualR build tools
+        <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">
+          {" "}
+          for developers
+        </span>
+      </h1>
+      <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate laboriosam eaque ducimus est consequatur amet, odio quis cupiditate hic? Dicta, quidem. Odit est suscipt
+      </p>
+      <div className="flex justify-center my-10">
+        <a
+          href="#"
+          className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md"
+        >
+          Start for free
+        </a>
+        <a href="#" className="py-3 px-4 mx-3 rounded-md border">
+          Documentation
+        </a>
+      </div>
+      <div className="md:flex w-4/5 md:w-full mt-10 justify-center">
+        <video
+          autoPlay
+          loop
+          muted
+          className="rounded-lg md:w-1/2 border border-orange-700 shadow-sm shadow-orange-400 mx-2 my-4 w-full"
+        >
+          <source src={video1} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <video
+          autoPlay
+          loop
+          muted
+          className="rounded-lg md:w-1/2 border border-orange-700 shadow-sm shadow-orange-400 mx-2 my-4  w-full"
+        >
+          <source src={video2} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
